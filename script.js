6756a4302e0d28f5f557fad904d0587d5812206e
Joshua Müller <119888496+youjoushi@users.noreply.github.com>
2022-12-25 19:57:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,10 @@
-/* 
-🌟 APP: Make Netflix
-
-Create a fetchMovies() function that will make a dynamic API call to what you need 👇
-========================================
-
-- fetchMovies()
-
-** fetchMovies takes in an URL, a div id or class from the HTML, and a path (poster or backdrop)
-
-
-
-These are the 3 main functions you must create 👇
-========================================
-
-- getOriginals()
-
-- getTrendingNow()
-
-- getTopRated()
-
-
-** These functions will provide the URL you need to fetch movies of that genere **
-
-These are all the DIV ID's you're gonna need access to 👇
-========================================================
-#1 CLASS 👉 'original__movies' = Div that holds Netflix Originals
-#2 ID 👉 'trending' = Div that holds trending Movies
-#3 ID 👉 'top_rated' = Div that holds top rated Movies
-*/
-
-// Call the main functions the page is loaded
 window.onload = () => {
     getOriginals()
     getTrendingNow()
     getTopRated()
   }
   
-  // ** Helper function that makes dynamic API calls **
-  // path_type 👉 (backdrop, poster)
-  // dom_element 👉 (trending, top rated)
-  // fetchMovies('https://api.themoviedb.org/3/movie/top_rated?api_key=19f84e11932abbc79e6d83f82d6d1045&language=en-US&page=1', 'top_rated', 'backdrop_path')
+
   function fetchMovies(url, dom_element, path_type) {
     fetch(url)
       .then(response => {
@@ -57,53 +22,45 @@ window.onload = () => {
       })
   }
   
-  //  ** Function that displays the movies to the DOM **
   showMovies = (movies, dom_element, path_type) => {
     
-    // Create a variable that grabs id or class
+
     var moviesEl = document.querySelector(dom_element)
   
-    // Loop through object
     for (var movie of movies.results) {
   
-      // Within loop create an img element
       var imageElement = document.createElement('img')
   
-      // Set attribute
       imageElement.setAttribute('data-id', movie.id)
   
-      // Set source
       imageElement.src = `https://image.tmdb.org/t/p/original${movie[path_type]}`
   
-      // Add event listener to handleMovieSelection() onClick
       imageElement.addEventListener('click', e => {
         handleMovieSelection(e)
       })
-      // Append the imageElement to the dom_element selected
+        
       moviesEl.appendChild(imageElement)
     }
   }
   
-  // ** Function that fetches Netflix Originals **
   function getOriginals() {
     var url =
       'https://api.themoviedb.org/3/discover/tv?api_key=19f84e11932abbc79e6d83f82d6d1045&with_networks=213'
     fetchMovies(url, '.original__movies', 'poster_path')
   }
-  // ** Function that fetches Trending Movies **
+
   function getTrendingNow() {
     var url =
       'https://api.themoviedb.org/3/trending/movie/week?api_key=19f84e11932abbc79e6d83f82d6d1045'
     fetchMovies(url, '#trending', 'backdrop_path')
   }
-  // ** Function that fetches Top Rated Movies **
+
   function getTopRated() {
     var url =
       'https://api.themoviedb.org/3/movie/top_rated?api_key=19f84e11932abbc79e6d83f82d6d1045&language=en-US&page=1'
     fetchMovies(url, '#top_rated', 'backdrop_path')
   }
   
-  // ** BONUS **
   
   async function getMovieTrailer(id) {
     var url = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=19f84e11932abbc79e6d83f82d6d1045&language=en-US`
@@ -145,9 +102,7 @@ window.onload = () => {
       setTrailer(youtubeTrailers)
     })
   
-    // open modal
     $('#trailerModal').modal('show')
-    // we need to call the api with the ID
   }
   
   
